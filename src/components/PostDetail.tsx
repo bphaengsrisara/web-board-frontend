4e338f5ed77f760ace11432ba720894ac16fc831
Bovonrajt Phaengsrisara <bovonrajt.p@gmail.com>
2024-12-03 13:41:29 +0700

    feat(post): add post detail relative datetime date

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -4,6 +4,9 @@ import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import Image from "next/image";
 import { Separator } from "./ui/separator";
 import { usePost } from "@/hooks/use-post";
+import { formatDistanceToNow } from "date-fns";
+import { useRouter } from "next/navigation";
+import { useCallback } from "react";
 
 interface PostDetailProps {
   pid: string;
@@ -11,6 +14,11 @@ interface PostDetailProps {
 
 export default function PostDetail({ pid }: Readonly<PostDetailProps>) {
   const { data: post, isFetching, error } = usePost(pid);
+  const { back } = useRouter();
+
+  const goBack = useCallback(() => {
+    back();
+  }, [back]);
 
   if (isFetching) {
     return (
@@ -36,14 +44,22 @@ export default function PostDetail({ pid }: Readonly<PostDetailProps>) {
     );
   }
 
-  const { author, title, content, topics, comments } = post;
+  const { author, title, content, topics, comments, updatedAt } = post;
 
   const imgSrc = `https://i.pravatar.cc/150?u=${author.id}`;
 
   return (
     <div className="flex flex-col gap-6">
       <div className="space-y-4 px-6">
-        <div className="flex items-center gap-4">
+        <Image
+          src="/images/arrow-left.svg"
+          alt="Back"
+          width={44}
+          height={44}
+          className="mb-10 cursor-pointer"
+          onClick={goBack}
+        />
+        <div className="flex items-center gap-2 font-inter">
           <Avatar>
             <AvatarImage asChild src={imgSrc}>
               <Image
@@ -57,8 +73,9 @@ export default function PostDetail({ pid }: Readonly<PostDetailProps>) {
               {author.username.charAt(0).toUpperCase()}
             </AvatarFallback>
           </Avatar>
-          <span className="text-sm font-medium text-grey-2">
-            {author.username}
+          <span className="text-sm font-medium">{author.username}</span>
+          <span className="mt-0.5 text-xs text-grey-2">
+            {formatDistanceToNow(updatedAt, { addSuffix: true })}
           </span>
         </div>
 
@@ -76,6 +93,20 @@ export default function PostDetail({ pid }: Readonly<PostDetailProps>) {
         <h1 className="text-xl font-semibold">{title}</h1>
 
         <p className="whitespace-pre-line text-sm text-gray-600">{content}</p>
+
+        <div className="flex cursor-pointer items-center gap-2 text-sm text-grey-2">
+          <Image
+            src="/images/comment.svg"
+            alt="Comments"
+            width={16}
+            height={16}
+            className="h-4 w-4"
+          />
+          <span>
+            {post.comments.length} Comment
+            {post.comments.length > 1 && "s"}
+          </span>
+        </div>
       </div>
 
       <Separator />
@@ -101,7 +132,12 @@ export default function PostDetail({ pid }: Readonly<PostDetailProps>) {
               </AvatarFallback>
             </Avatar>
             <div className="flex-1">
-              <p className="text-sm font-medium">{comment.author.username}</p>
+              <div className="flex items-center gap-2">
+                <p className="text-sm font-medium">{comment.author.username}</p>
+                <span className="mt-0.5 text-xs text-grey-2">
+                  {formatDistanceToNow(comment.updatedAt, { addSuffix: true })}
+                </span>
+              </div>
               <p className="text-sm text-gray-600">{comment.content}</p>
             </div>
           </div>
